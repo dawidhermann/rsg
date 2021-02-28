5a2dac5ba7aa64d40e4d873e56dc97a9b20656de
Dawid <hermann.dawid.91@gmnail.com>
2021-02-28 19:45:36 +0100

    Logger middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,16 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RsgModule } from './rsg/rsg.module';
+import { LoggerMiddleware } from './logger.middleware';
 
 @Module({
   imports: [RsgModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer): any {
+    consumer.apply(LoggerMiddleware).forRoutes('rsg');
+  }
+}
